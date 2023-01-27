0d6a24df18b790f0a083f91fb3888d6fb26bb33e
DevRefaul <devrefaul@gmail.com>
2023-01-27 22:35:32 +0600

    added google font to website

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 function App() {
   return (
     <div>
-      <h2 className="text-4xl font-semibold">This is ecommerce website</h2>
+      <h2 className="text-4xl font-semibold font-[Roboto Slab]">This is ecommerce website</h2>
     </div>
   );
 }
