039e11c959801d8a9ae4c79f343af652b2b13fd8
DevRefaul <devrefaul@gmail.com>
2023-01-29 11:25:51 +0600

    github link added in footer and some changes are made

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import Footer from "../Components/Shared/Footer/Footer";
 import Header from "../Components/Shared/Navbar/Header";
 
 const Root = () => {
@@ -7,6 +8,7 @@ const Root = () => {
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
