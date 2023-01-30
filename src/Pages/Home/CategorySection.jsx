410e1372a753b505745b5e97f305a0f8f542375f
DevRefaul <devrefaul@gmail.com>
2023-01-30 21:20:32 +0600

    added one more category

--- src/Pages/Home/CategorySection.jsx
+++ src/Pages/Home/CategorySection.jsx
@@ -7,18 +7,18 @@ const CategorySection = () => {
         Shop By Category
       </h2>
 
-      <div className="my-8 grid grid-cols-2 lg:grid-cols-3 gap-4">
-        <div className="bg-green-400">
-          {" "}
+      <div className="my-8 grid grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="">
           <h2>Fashion</h2>
         </div>
         <div className="bg-green-400">
-          {" "}
           <h2>Furniture</h2>
         </div>
         <div className="bg-green-400">
-          {" "}
           <h2>Electronics</h2>
+        </div>
+        <div className="bg-green-400">
+          <h2>Plant</h2>
         </div>
       </div>
     </section>
