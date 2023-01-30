09a60bf136a46f1822ba9b85944146314a80ccd2
DevRefaul <devrefaul@gmail.com>
2023-01-30 21:33:07 +0600

    color added in category card

--- src/Pages/Home/CategorySection.jsx
+++ src/Pages/Home/CategorySection.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./CategorySection.css";
 
 const CategorySection = () => {
   return (
@@ -7,18 +8,26 @@ const CategorySection = () => {
         Shop By Category
       </h2>
 
-      <div className="my-8 grid grid-cols-2 lg:grid-cols-4 gap-4">
-        <div className="">
-          <h2>Fashion</h2>
+      <div className="my-8 grid md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div className="bg-gradient-to-r from-violet-500 to-fuchsia-500 h-32 relative rounded-lg">
+          <h2 className="textCenter text-white text-3xl font-medium font-['Roboto_Slab']">
+            Fashion
+          </h2>
         </div>
-        <div className="bg-green-400">
-          <h2>Furniture</h2>
+        <div className="bg-gradient-to-r from-rose-400 to-orange-300 h-32 relative rounded-lg">
+          <h2 className="textCenter text-white text-3xl font-medium font-['Roboto_Slab']">
+            Furniture
+          </h2>
         </div>
-        <div className="bg-green-400">
-          <h2>Electronics</h2>
+        <div className="bg-gradient-to-r from-blue-700 via-blue-800 to-gray-900 h-32 relative rounded-lg">
+          <h2 className="textCenter text-white text-3xl font-medium font-['Roboto_Slab']">
+            Electronics
+          </h2>
         </div>
-        <div className="bg-green-400">
-          <h2>Plant</h2>
+        <div className="bg-gradient-to-r from-green-300 via-green-400 to-green-500 h-32 relative rounded-lg">
+          <h2 className="textCenter text-white text-3xl font-medium font-['Roboto_Slab']">
+            Plant
+          </h2>
         </div>
       </div>
     </section>
