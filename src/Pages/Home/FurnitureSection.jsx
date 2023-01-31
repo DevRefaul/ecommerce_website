f2e0237defd6bdd1fc9c27ae5969f51b26630a4e
DevRefaul <devrefaul@gmail.com>
2023-01-31 20:43:58 +0600

    created furniture section

---
+++ src/Pages/Home/FurnitureSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FurnitureSection = () => {
+  return (
+    <section className="my-6">
+      <h2 className="text-2xl font-semibold">Furnitures</h2>
+    </section>
+  );
+};
+
+export default FurnitureSection;
