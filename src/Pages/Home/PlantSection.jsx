f9ed8ce9576831a30faa884518122d22a29a6544
DevRefaul <devrefaul@gmail.com>
2023-01-31 20:46:00 +0600

    created plants section for homepage

---
+++ src/Pages/Home/PlantSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const PlantSection = () => {
+  return (
+    <section className="my-6">
+      <h2 className="text-2xl font-semibold">Plants</h2>
+    </section>
+  );
+};
+
+export default PlantSection;
