186510fe0b64ad253de6e3337e96f6eae602cc57
DevRefaul <devrefaul@gmail.com>
2023-01-31 20:40:41 +0600

    created electronics section on homepage

---
+++ src/Pages/Home/ElectronicsSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ElectronicsSection = () => {
+  return (
+    <section className="my-6">
+      <h2 className="text-2xl font-semibold">Electronics</h2>
+    </section>
+  );
+};
+
+export default ElectronicsSection;
