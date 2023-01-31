67b42ccde4d1495018930b10ca217701c24e46ec
DevRefaul <devrefaul@gmail.com>
2023-01-31 20:35:00 +0600

    created fashionsection for homepage

---
+++ src/Pages/Home/FashionSection.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const FashionSection = () => {
+  return (
+    <div>
+      <h2>This is Fashion section on homepage</h2>
+    </div>
+  );
+};
+
+export default FashionSection;
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Banner from "./Banner";
 import CategorySection from "./CategorySection";
+import FashionSection from "./FashionSection";
 
 const Home = () => {
   return (
     <div className="w-[95%] md:w-[80%] mx-auto">
       <Banner />
       <CategorySection />
+      <FashionSection />
     </div>
   );
 };
