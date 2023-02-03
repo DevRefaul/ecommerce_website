2947d1bedd3c16e5513a39805ebd9996777ce110
DevRefaul <devrefaul@gmail.com>
2023-02-03 20:04:34 +0600

    link added for contact us page on navbar

--- src/Components/Shared/Navbar/Header.jsx
+++ src/Components/Shared/Navbar/Header.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import logo from "../../../Assets/shopaholic.png";
 // import { Link } from "react-router-dom";
 
@@ -134,12 +135,12 @@ const Header = () => {
                 </a>
               </li>
               <li>
-                <a
-                  href="/"
+                <Link
+                  to="/contact"
                   className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                 >
                   Contact
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
