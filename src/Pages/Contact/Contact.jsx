06537764bf227b9dd7a33aee39b706d764f2a835
DevRefaul <devrefaul@gmail.com>
2023-02-03 20:30:50 +0600

    minimalistic contact us page is partially done

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -2,9 +2,61 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <h2>This is contact us page</h2>
-    </div>
+    <section className="lg:flex justify-center items-center h-[100vh] overflow-scroll">
+      <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-800 dark:text-gray-100">
+        <div className="flex flex-col justify-between">
+          <div className="space-y-2"></div>
+          <img
+            src="assets/svg/doodle.svg"
+            alt=""
+            className="p-6 h-52 md:h-64"
+          />
+        </div>
+        <form
+          novalidate=""
+          className="space-y-6 ng-untouched ng-pristine ng-valid"
+        >
+          <div>
+            <label for="name" className="text-sm">
+              Full name
+            </label>
+            <input
+              id="name"
+              type="text"
+              placeholder=""
+              className="w-full p-3 rounded dark:bg-gray-800"
+            />
+          </div>
+          <div>
+            <label for="email" className="text-sm">
+              Email
+            </label>
+            <input
+              id="email"
+              type="email"
+              className="w-full p-3 rounded dark:bg-gray-800"
+            />
+          </div>
+          <div>
+            <label for="message" className="text-sm">
+              Message
+            </label>
+            <textarea
+              id="message"
+              rows="3"
+              className="w-full p-3 rounded dark:bg-gray-800"
+            ></textarea>
+          </div>
+          <button
+            type="submit"
+            className="w-full p-3 bg-green-300 hover:bg-transparent border-2 border-transparent hover:border-green-400
+           text-sm font-bold tracking-wide uppercase rounded"
+          >
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
   );
 };
 
