c955fd32bb704477bb6891447ee01f45c1a78468
DevRefaul <devrefaul@gmail.com>
2023-02-03 20:56:18 +0600

    added lottie animation to contact us page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,60 +1,60 @@
 import React from "react";
+import Lottie from "lottie-react";
+import contactAnimation from "./contact-us.json";
 
 const Contact = () => {
   return (
     <section className="lg:flex justify-center items-center h-[100vh] overflow-scroll">
-      <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-800 dark:text-gray-100">
-        <div className="flex flex-col justify-between">
-          <div className="space-y-2"></div>
-          <img
-            src="assets/svg/doodle.svg"
-            alt=""
-            className="p-6 h-52 md:h-64"
-          />
-        </div>
-        <form
-          novalidate=""
-          className="space-y-6 ng-untouched ng-pristine ng-valid"
-        >
-          <div>
-            <label for="name" className="text-sm">
-              Full name
-            </label>
-            <input
-              id="name"
-              type="text"
-              placeholder=""
-              className="w-full p-3 rounded dark:bg-gray-800"
-            />
+      <div>
+        <h2 className="text-2xl font-semibold text-center">Contact us</h2>
+        <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-800 dark:text-gray-100">
+          <div className="flex justify-center items-center">
+            <Lottie animationData={contactAnimation} />
           </div>
-          <div>
-            <label for="email" className="text-sm">
-              Email
-            </label>
-            <input
-              id="email"
-              type="email"
-              className="w-full p-3 rounded dark:bg-gray-800"
-            />
-          </div>
-          <div>
-            <label for="message" className="text-sm">
-              Message
-            </label>
-            <textarea
-              id="message"
-              rows="3"
-              className="w-full p-3 rounded dark:bg-gray-800"
-            ></textarea>
-          </div>
-          <button
-            type="submit"
-            className="w-full p-3 bg-green-300 hover:bg-transparent border-2 border-transparent hover:border-green-400
-           text-sm font-bold tracking-wide uppercase rounded"
+          <form
+            novalidate=""
+            className="space-y-6 ng-untouched ng-pristine ng-valid"
           >
-            Send Message
-          </button>
-        </form>
+            <div>
+              <label for="name" className="text-sm">
+                Full name
+              </label>
+              <input
+                id="name"
+                type="text"
+                placeholder=""
+                className="w-full p-3 rounded dark:bg-gray-800"
+              />
+            </div>
+            <div>
+              <label for="email" className="text-sm">
+                Email
+              </label>
+              <input
+                id="email"
+                type="email"
+                className="w-full p-3 rounded dark:bg-gray-800"
+              />
+            </div>
+            <div>
+              <label for="message" className="text-sm">
+                Message
+              </label>
+              <textarea
+                id="message"
+                rows="3"
+                className="w-full p-3 rounded dark:bg-gray-800"
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              className="w-full p-3 bg-green-300 hover:bg-transparent border-2 border-transparent hover:border-green-400
+           text-sm font-bold tracking-wide uppercase rounded"
+            >
+              Send Message
+            </button>
+          </form>
+        </div>
       </div>
     </section>
   );
