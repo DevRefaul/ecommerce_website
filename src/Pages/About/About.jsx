cef053cc01b6d077fa138afd96f2b66dec2795b0
DevRefaul <devrefaul@gmail.com>
2023-02-03 21:10:08 +0600

    added minimalist texts on about us page

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -2,8 +2,32 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>This is about us page</h2>
+    <div className="px-6 py-4">
+      <h1 className="text-2xl font-bold mb-2">About MyShop</h1>
+      <p className="mb-4 text-gray-700">
+        MyShop is a leading e-commerce platform that provides customers with a
+        seamless and convenient shopping experience. We offer a wide range of
+        products across various categories, ensuring that our customers have
+        access to everything they need in one place.
+      </p>
+      <p className="mb-4 text-gray-700">
+        Our team is dedicated to providing the best customer service possible,
+        and we are always striving to improve our platform to meet the
+        ever-evolving needs of our customers. We believe that shopping should be
+        an enjoyable experience, and we work hard to make it so for everyone who
+        visits MyShop.
+      </p>
+      <p className="mb-4 text-gray-700">
+        We are committed to providing our customers with the best possible value
+        for their money, which is why we offer competitive prices, fast and
+        reliable shipping, and a no-questions-asked returns policy. Whether
+        you're shopping for yourself or for someone else, you can rest assured
+        that MyShop is the best choice for all your shopping needs.
+      </p>
+      <p className="mb-4 text-gray-700">
+        So what are you waiting for? Start shopping with MyShop today and
+        discover the ease and convenience of online shopping!
+      </p>
     </div>
   );
 };
