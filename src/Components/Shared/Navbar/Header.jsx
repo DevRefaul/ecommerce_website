b87b236aba6bf650476cc831ff9708caa81d73d7
DevRefaul <devrefaul@gmail.com>
2023-02-03 21:51:34 +0600

    created submenu for products on navbar

--- src/Components/Shared/Navbar/Header.jsx
+++ src/Components/Shared/Navbar/Header.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../../Assets/shopaholic.png";
 // import { Link } from "react-router-dom";
 
 const Header = () => {
+  const [dropdown, setDropdown] = useState(true);
+
+  const handleDropDownMenu = () => {
+    if (dropdown) {
+      document.getElementById("dropdownNavbar").classList.remove("hidden");
+    } else {
+      document.getElementById("dropdownNavbar").classList.add("hidden");
+    }
+    setDropdown(!dropdown);
+  };
+  const handleLetSubmenuHide = () => {
+    document.getElementById("dropdownNavbar").classList.add("hidden");
+    setDropdown(!dropdown);
+  };
+
   return (
     <>
-      <nav className="bg-[#90ee90] border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+      <nav className="bg-[#90ee90] border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 z-10">
         <div className="container flex flex-wrap items-center justify-between mx-auto">
           <a href="/" className="flex items-center">
             <img src={logo} alt="" className="h-6 mr-3 sm:h-7" />
@@ -127,12 +142,79 @@ const Header = () => {
                 </a>
               </li>
               <li>
-                <Link
-                  to="/products"
-                  className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                <button
+                  id="dropdownNavbarLink"
+                  onClick={handleDropDownMenu}
+                  data-dropdown-toggle="dropdownNavbar"
+                  className="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-gray-400 dark:hover:text-white dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent relative"
                 >
-                  Products
-                </Link>
+                  Products{" "}
+                  <svg
+                    className="w-5 h-5 ml-1"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    ></path>
+                  </svg>
+                </button>
+                {/* <!-- Dropdown menu --> */}
+                <div
+                  id="dropdownNavbar"
+                  className="z-50 absolute top-14 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"
+                >
+                  <ul
+                    className="py-2 text-sm text-gray-700 dark:text-gray-400"
+                    aria-labelledby="dropdownLargeButton"
+                    onClick={handleLetSubmenuHide}
+                  >
+                    <li>
+                      <Link
+                        to="/products"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        All Products
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/products/fashion"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        Fashion
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/products/electronics"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        Electronics
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/products/furniture"
+                        className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                      >
+                        Furnitures
+                      </Link>
+                    </li>
+                    <li>
+                      <Link
+                        to="/products/plant"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-400 dark:hover:text-white"
+                      >
+                        Plants
+                      </Link>
+                    </li>
+                  </ul>
+                </div>
               </li>
               <li>
                 <Link
