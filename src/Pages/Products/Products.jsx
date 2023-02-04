4cf8f497596eb304725f8247770e0b02052e8e17
DevRefaul <devrefaul@gmail.com>
2023-02-04 13:40:45 +0600

    all products page is updated

--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -5,7 +5,16 @@ const Products = () => {
   return (
     <section className="w-[95%] md:w-[80%] mx-auto my-8">
       <h2 className="text-3xl font-semibold my-6 text-center">All Products</h2>
-      <SingleCardForAllProducts />
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 place-items-center">
+        <SingleCardForAllProducts />
+        <SingleCardForAllProducts />
+        <SingleCardForAllProducts />
+        <SingleCardForAllProducts />
+        <SingleCardForAllProducts />
+        <SingleCardForAllProducts />
+        <SingleCardForAllProducts />
+      </div>
     </section>
   );
 };
