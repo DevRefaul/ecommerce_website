7a2b5ea60316512d4e5a1b6e3197d78f3b808315
DevRefaul <devrefaul@gmail.com>
2023-02-04 18:53:18 +0600

    added a utils witch is api

--- src/Components/SingleCardComponents/SingleCardForAllProducts.jsx
+++ src/Components/SingleCardComponents/SingleCardForAllProducts.jsx
@@ -1,29 +1,35 @@
 import { Badge, Card } from "flowbite-react";
 import React from "react";
+import { Link } from "react-router-dom";
+import "./SingleCard.css";
 
-const SingleCardForAllProducts = () => {
+const SingleCardForAllProducts = ({ product }) => {
+  const { name, price, description, image, brand } = product;
+
   return (
     <>
-      <div className="max-w-sm">
-        <Card imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
+      <div className="max-w-sm h-full singleCard">
+        <Card imgSrc={image && image} className="h-full">
           <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white flex justify-between">
-            Noteworthy technology acquisitions 2021
+            {name}
             <Badge color="success" size="sm">
-              Brand
+              {brand}
             </Badge>
           </h5>
 
           <p className="font-normal text-gray-700 dark:text-gray-400">
-            Description here
+            {description}
           </p>
 
-          <h4 className="text-xl font-bold">$200</h4>
+          <h4 className="text-xl font-bold">${price}</h4>
 
           {/* section for price */}
           <div className="grid grid-cols-2 gap-4">
-            <button className="text-white bg-gradient-to-r from-green-300 to-blue-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-6 w-full">
-              See More
-            </button>
+            <Link to="/">
+              <button className="text-white bg-gradient-to-r from-green-300 to-blue-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-6 w-full">
+                See More
+              </button>
+            </Link>
             <button className="text-white bg-gradient-to-r from-orange-400 to-yellow-300 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-6 w-full">
               Add To Cart
             </button>
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,19 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SingleCardForAllProducts from "../../Components/SingleCardComponents/SingleCardForAllProducts";
+// import SingleCardForAllProducts from "../../Components/SingleCardComponents/SingleCardForAllProducts";
 
 const Products = () => {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("electronics.json")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setData(data.products);
+      })
+      .catch((err) => console.error(err.message));
+  }, []);
+
   return (
     <section className="w-[95%] md:w-[80%] mx-auto my-8">
       <h2 className="text-3xl font-semibold my-6 text-center">All Products</h2>
 
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 place-items-center">
-        <SingleCardForAllProducts />
-        <SingleCardForAllProducts />
-        <SingleCardForAllProducts />
-        <SingleCardForAllProducts />
-        <SingleCardForAllProducts />
-        <SingleCardForAllProducts />
-        <SingleCardForAllProducts />
+        {data
+          ? data.map((product, i) => {
+              return <SingleCardForAllProducts key={i} product={product} />;
+            })
+          : ""}
       </div>
     </section>
   );
