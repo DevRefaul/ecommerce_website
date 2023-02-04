80f0ca862089b978a457f81fa39f41e1efeffb04
DevRefaul <devrefaul@gmail.com>
2023-02-04 12:58:05 +0600

    created a single card component for products

---
+++ src/Components/SingleCardComponents/SingleCardForAllProducts.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SingleCardForAllProducts = () => {
+  return (
+    <div>
+      <h2>This is a single card for all products</h2>
+    </div>
+  );
+};
+
+export default SingleCardForAllProducts;
