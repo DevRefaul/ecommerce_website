089f2838f7ecbca2eeba15d5b1599afe1fe2cf31
DevRefaul <devrefaul@gmail.com>
2023-02-04 13:53:07 +0600

    added some style on singlecard for all products

--- src/Components/SingleCardComponents/SingleCardForAllProducts.jsx
+++ src/Components/SingleCardComponents/SingleCardForAllProducts.jsx
@@ -16,6 +16,18 @@ const SingleCardForAllProducts = () => {
           <p className="font-normal text-gray-700 dark:text-gray-400">
             Description here
           </p>
+
+          <h4 className="text-xl font-bold">$</h4>
+
+          {/* section for price */}
+          <div className="grid grid-cols-2 gap-4">
+            <button className="text-white bg-gradient-to-r from-green-300 to-blue-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-6 w-full">
+              See More
+            </button>
+            <button className="text-white bg-gradient-to-r from-orange-400 to-yellow-300 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-6 w-full">
+              Add To Cart
+            </button>
+          </div>
         </Card>
       </div>
     </>
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -6,7 +6,7 @@ const Products = () => {
     <section className="w-[95%] md:w-[80%] mx-auto my-8">
       <h2 className="text-3xl font-semibold my-6 text-center">All Products</h2>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 place-items-center">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 place-items-center">
         <SingleCardForAllProducts />
         <SingleCardForAllProducts />
         <SingleCardForAllProducts />
