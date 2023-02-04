9f72df80e278e2f47aaacfcef442d3a92ce81d15
DevRefaul <devrefaul@gmail.com>
2023-02-04 13:35:22 +0600

    some changes are made on singlecard for products

--- src/Components/SingleCardComponents/SingleCardForAllProducts.jsx
+++ src/Components/SingleCardComponents/SingleCardForAllProducts.jsx
@@ -1,10 +1,24 @@
+import { Badge, Card } from "flowbite-react";
 import React from "react";
 
 const SingleCardForAllProducts = () => {
   return (
-    <div>
-      <h2>This is a single card for all products</h2>
-    </div>
+    <>
+      <div className="max-w-sm">
+        <Card imgSrc="https://flowbite.com/docs/images/blog/image-1.jpg">
+          <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white flex justify-between">
+            Noteworthy technology acquisitions 2021
+            <Badge color="success" size="sm">
+              Brand
+            </Badge>
+          </h5>
+
+          <p className="font-normal text-gray-700 dark:text-gray-400">
+            Description here
+          </p>
+        </Card>
+      </div>
+    </>
   );
 };
 
--- src/Pages/Products/Products.jsx
+++ src/Pages/Products/Products.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import SingleCardForAllProducts from "../../Components/SingleCardComponents/SingleCardForAllProducts";
 
 const Products = () => {
   return (
-    <div>
-      <h2>THis is main products page</h2>
-    </div>
+    <section className="w-[95%] md:w-[80%] mx-auto my-8">
+      <h2 className="text-3xl font-semibold my-6 text-center">All Products</h2>
+      <SingleCardForAllProducts />
+    </section>
   );
 };
 
