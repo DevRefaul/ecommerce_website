3061739443ba0bfa7e6b86504d95303aeadcd82e
DevRefaul <devrefaul@gmail.com>
2023-03-02 12:16:17 +0600

    changes in the electronics section of home

--- src/Pages/Home/ElectronicsSection.jsx
+++ src/Pages/Home/ElectronicsSection.jsx
@@ -27,7 +27,7 @@ const ElectronicsSection = () => {
     <section className="my-6">
       <h2 className="text-3xl font-bold my-8">Electronics</h2>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 place-items-center">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4 place-items-center">
         {data
           ? data.map((product, i) => {
               return <SingleCardForAllProducts key={i} product={product} />;
