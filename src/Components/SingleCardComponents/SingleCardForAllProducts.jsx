46eac9ed7db0a5b878a16fc3bea5bcf4e61b587c
DevRefaul <devrefaul@gmail.com>
2023-03-02 12:30:50 +0600

    updated the card of product

--- src/Components/SingleCardComponents/SingleCardForAllProducts.jsx
+++ src/Components/SingleCardComponents/SingleCardForAllProducts.jsx
@@ -10,21 +10,23 @@ const SingleCardForAllProducts = ({ product }) => {
     <>
       <div className="max-w-sm h-full singleCard">
         <Card imgSrc={image && image} className="h-full">
-          <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white flex justify-between">
-            {name}
-            <Badge color="success" size="sm">
-              {brand}
-            </Badge>
-          </h5>
+          <div>
+            <h5 className="text-xl font-bold tracking-tight text-gray-900 dark:text-white flex justify-between">
+              {name.slice(0, 20) + "..."}
+              <Badge color="success" size="sm">
+                {brand}
+              </Badge>
+            </h5>
 
-          <p className="font-normal text-gray-700 dark:text-gray-400">
-            {description}
-          </p>
+            <p className="font-normal text-gray-700 dark:text-gray-400">
+              {description.slice(0, 50) + "..."}
+            </p>
 
-          <h4 className="text-xl font-bold">${price}</h4>
+            <h4 className="text-xl font-bold">${price}</h4>
+          </div>
 
           {/* section for price */}
-          <div className="grid xl:grid-cols-2 xl:gap-4">
+          <div className="grid lg:grid-cols-2 lg:gap-4">
             <Link to={`/products/${name}`} state={{ productName: name }}>
               <button className="text-white bg-gradient-to-r from-green-300 to-blue-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 lg:mt-6 w-full">
                 See More
