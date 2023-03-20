2bb8c36ead47cb2ad2c0fb0bbca42c4b5d8632ca
DevRefaul <devrefaul@gmail.com>
2023-03-20 21:36:51 +0600

    width and height are changed for span

--- src/Components/LoadingScene/LoadingScene.jsx
+++ src/Components/LoadingScene/LoadingScene.jsx
@@ -5,7 +5,7 @@ const LoadingScene = () => {
     <div className="h-screen flex justify-center items-center">
       <h2 className="text-6xl uppercase">
         L
-        <span className="border-[5px] w-[50px] h-[50px] border-green-400 border-dashed rounded-full inline-block"></span>
+        <span className="border-[4px] w-[45px] h-[45px] border-green-400 border-dashed rounded-full inline-block"></span>
         ading Data
       </h2>
     </div>
