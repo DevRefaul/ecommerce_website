161f06081a4b4e03a43029773593e05a6ee9c0db
DevRefaul <devrefaul@gmail.com>
2023-03-20 21:41:02 +0600

    loading scene component is done

--- src/Components/LoadingScene/LoadingScene.jsx
+++ src/Components/LoadingScene/LoadingScene.jsx
@@ -4,9 +4,9 @@ import "./LoadingScene.css";
 const LoadingScene = () => {
   return (
     <div className="h-screen flex justify-center items-center">
-      <h2 className="text-6xl uppercase">
+      <h2 className="text-6xl uppercase font-semibold">
         L
-        <span className="loader border-[4px] w-[45px] h-[45px] border-green-400 border-dashed rounded-full inline-block"></span>
+        <span className="loader border-[6px] w-[45px] h-[45px] border-green-400 border-dashed rounded-full inline-block"></span>
         ading Data
       </h2>
     </div>
