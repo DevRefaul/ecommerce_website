b556d8343e66f90ab0cd2f92c45babd7ad8c9f93
DevRefaul <devrefaul@gmail.com>
2023-03-20 19:40:38 +0600

    loading data page is finished for now

--- src/Components/LoadingScene/LoadingScene.jsx
+++ src/Components/LoadingScene/LoadingScene.jsx
@@ -4,7 +4,9 @@ const LoadingScene = () => {
   return (
     <div className="h-screen flex justify-center items-center">
       <h2 className="text-6xl uppercase">
-        L<span className="text-green-500 font-bold">o</span>ading Data
+        L
+        <span className="border-[5px] w-[50px] h-[50px] border-green-400 border-dashed rounded-full inline-block"></span>
+        ading Data
       </h2>
     </div>
   );
