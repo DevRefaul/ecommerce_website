13830593708ce04fef4c41805af3523c248fc173
DevRefaul <devrefaul@gmail.com>
2023-03-20 19:35:52 +0600

    updated the loading page font size

--- src/Components/LoadingScene/LoadingScene.jsx
+++ src/Components/LoadingScene/LoadingScene.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const LoadingScene = () => {
   return (
     <div className="h-screen flex justify-center items-center">
-      <h2 className="text-4xl">
+      <h2 className="text-6xl uppercase">
         L<span className="text-green-500 font-bold">o</span>ading Data
       </h2>
     </div>
