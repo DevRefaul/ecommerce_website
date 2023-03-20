b7cac205ccf20dc476730b1ae5da086fb9e92cda
DevRefaul <devrefaul@gmail.com>
2023-03-20 19:31:14 +0600

    added some changes in the loading scene

--- src/Components/LoadingScene/LoadingScene.jsx
+++ src/Components/LoadingScene/LoadingScene.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const LoadingScene = () => {
   return (
-    <div>
+    <div className="h-screen flex justify-center items-center">
       <h2>This is loading scene</h2>
     </div>
   );
