aef805e53336ddf5b8c7991e6e0d0846a62f4168
DevRefaul <devrefaul@gmail.com>
2023-03-22 16:07:51 +0600

    created another section for the home page which is partner brands

---
+++ src/Pages/Home/BrandsPartner.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BrandsPartner = () => {
+  return (
+    <section className="my-8">
+      <h2 className="text-2xl font-semibold text-center">Our Brand Partners</h2>
+    </section>
+  );
+};
+
+export default BrandsPartner;
