125125786bb68195a302f4a5316a9ea528351496
DevRefaul <devrefaul@gmail.com>
2023-03-24 19:45:35 +0600

    password input in login page is updated

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -37,7 +37,7 @@ const Login = () => {
                             placeholder="Username"
                           />
                           <label
-                            for="exampleFormControlInput1"
+                            for="userName"
                             className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-blue-600 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
                           >
                             Username
@@ -50,11 +50,11 @@ const Login = () => {
                           <input
                             type="password"
                             className="peer block min-h-[auto] w-full rounded border bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                            id="exampleFormControlInput11"
+                            id="password"
                             placeholder="Password"
                           />
                           <label
-                            for="exampleFormControlInput11"
+                            for="password"
                             className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-blue-600 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
                           >
                             Password
