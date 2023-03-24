c837c385c370a591b4ac34e6f1f3a959f19861e7
DevRefaul <devrefaul@gmail.com>
2023-03-24 19:44:01 +0600

    added function for getting login data

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -5,6 +5,9 @@ import logo from "../../Assets/shopaholic.png";
 import loginAnimation from "./loginAnimation.json";
 
 const Login = () => {
+
+  const handleLoginUser = () => {};
+
   return (
     <>
       <section className="gradient-form h-full bg-white">
@@ -59,6 +62,7 @@ const Login = () => {
                         </div>
                         <div className="mb-12 pt-1 pb-1 text-center">
                           <button
+                            onClick={handleLoginUser}
                             className="mb-3 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] bg-gradient-to-r from-sky-400 to-cyan-300"
                             type="button"
                           >
