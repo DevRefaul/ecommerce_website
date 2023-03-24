a4f39247b3b8e073f5bc0ae79228ea092ace5f68
DevRefaul <devrefaul@gmail.com>
2023-03-24 19:46:57 +0600

    updated username to email for login input

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -6,7 +6,9 @@ import loginAnimation from "./loginAnimation.json";
 
 const Login = () => {
 
-  const handleLoginUser = () => {};
+  const handleLoginUser = () => {
+    const username = document.getElementById("");
+  };
 
   return (
     <>
@@ -33,14 +35,14 @@ const Login = () => {
                           <input
                             type="text"
                             className="peer block min-h-[auto] w-full rounded border bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                            id="userName"
-                            placeholder="Username"
+                            id="email"
+                            placeholder="Email"
                           />
                           <label
-                            for="userName"
+                            for="email"
                             className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-blue-600 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
                           >
-                            Username
+                            Email
                           </label>
                         </div>
                         <div
