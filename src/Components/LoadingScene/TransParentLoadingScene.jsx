a14b4dd68bd9fda44bbfc66f04c9535a51dbb78a
DevRefaul <devrefaul@gmail.com>
2023-03-25 15:49:40 +0600

    transparent leading scene is finalized

--- src/Components/LoadingScene/TransParentLoadingScene.jsx
+++ src/Components/LoadingScene/TransParentLoadingScene.jsx
@@ -1,27 +1,12 @@
 import React from "react";
-import { ThreeCircles } from "react-loader-spinner";
 import "./LoadingScene.css";
 
 const TransParentLoadingScene = () => {
   return (
     <section className="flex justify-center items-center h-screen bg-transparent">
-      <h2 className="text-6xl uppercase text-[#4fa94d] font-semibold flex">
+      <h2 className="text-6xl uppercase font-semibold flex">
         L
-        <span className="inline-block mx-2">
-          {" "}
-          <ThreeCircles
-            height="48"
-            width="48"
-            color="#4fa94d"
-            wrapperStyle={{}}
-            wrapperClass=""
-            visible={true}
-            ariaLabel="three-circles-rotating"
-            outerCircleColor=""
-            innerCircleColor=""
-            middleCircleColor=""
-          />
-        </span>
+        <span className="loader mx-1 border-[6px] w-[50px] h-[50px] border-[#4fa94d] border-dashed rounded-full inline-block"></span>
         ading
       </h2>
     </section>
