78fa6a63c765ab6f406a76dd321584ff08f59a6b
DevRefaul <devrefaul@gmail.com>
2023-03-25 15:36:08 +0600

    completed transparent loading scene component

--- src/Components/LoadingScene/TransParentLoadingScene.jsx
+++ src/Components/LoadingScene/TransParentLoadingScene.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { ThreeCircles } from "react-loader-spinner";
 
 const TransParentLoadingScene = () => {
-  return <div></div>;
+  return (
+    <section className="flex justify-center items-center h-screen bg-[rgba(0,0,0,0.3)]">
+      <ThreeCircles
+        height="100"
+        width="100"
+        color="#4fa94d"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="three-circles-rotating"
+        outerCircleColor=""
+        innerCircleColor=""
+        middleCircleColor=""
+      />
+    </section>
+  );
 };
 
 export default TransParentLoadingScene;
