075f07ef68625323dae705391cf71a90a59fa3b2
DevRefaul <devrefaul@gmail.com>
2023-03-25 18:03:18 +0600

    added this functionality for every input

--- src/Pages/SignUp/SignUpPage.jsx
+++ src/Pages/SignUp/SignUpPage.jsx
@@ -109,9 +109,19 @@ const SignUpPage = () => {
                             className="peer block min-h-[auto] w-full rounded border bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                             id="address"
                             placeholder="Address"
+                            onChange={(e) =>
+                              e.target.value
+                                ? document
+                                    .getElementById("addressLabel")
+                                    .classList.add("hidden")
+                                : document
+                                    .getElementById("addressLabel")
+                                    .classList.remove("hidden")
+                            }
                           />
                           <label
                             htmlFor="address"
+                            id="addressLabel"
                             className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-blue-600 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
                           >
                             Address
@@ -126,9 +136,19 @@ const SignUpPage = () => {
                             className="peer block min-h-[auto] w-full rounded border bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                             id="firstPass"
                             placeholder="Password"
+                            onChange={(e) =>
+                              e.target.value
+                                ? document
+                                    .getElementById("passLabel")
+                                    .classList.add("hidden")
+                                : document
+                                    .getElementById("passLabel")
+                                    .classList.remove("hidden")
+                            }
                           />
                           <label
                             htmlFor="firstPass"
+                            id="passLabel"
                             className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-blue-600 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
                           >
                             Password
@@ -143,9 +163,19 @@ const SignUpPage = () => {
                             className="peer block min-h-[auto] w-full rounded border bg-transparent py-[0.32rem] px-3 leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
                             id="secondPass"
                             placeholder="Password"
+                            onChange={(e) =>
+                              e.target.value
+                                ? document
+                                    .getElementById("confirmPassLabel")
+                                    .classList.add("hidden")
+                                : document
+                                    .getElementById("confirmPassLabel")
+                                    .classList.remove("hidden")
+                            }
                           />
                           <label
                             htmlFor="secondPass"
+                            id="confirmPassLabel"
                             className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-blue-600 peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-neutral-200"
                           >
                             Confirm Password
