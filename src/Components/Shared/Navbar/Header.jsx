f074dd552df68c0ad9082f9b5db077816a61f2e5
DevRefaul <devrefaul@gmail.com>
2023-03-27 15:21:57 +0600

    added conditional rendering for login and profile section

--- src/Components/Shared/Navbar/Header.jsx
+++ src/Components/Shared/Navbar/Header.jsx
@@ -104,13 +104,113 @@ const Header = () => {
             </button>
 
             {/* login and profile */}
-            <div className="hidden lg:block">
-              <Link to="/login">
-                <button className="py-2.5 px-5 ml-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
-                  Login
-                </button>
-              </Link>
-            </div>
+
+            {localStorage.getItem("UserLoggedIn") === "true" ? (
+              <>
+                <div className="flex items-center md:order-2 ml-4">
+                  <button
+                    type="button"
+                    className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                    id="user-menu-button"
+                    aria-expanded="false"
+                    data-dropdown-toggle="user-dropdown"
+                    data-dropdown-placement="bottom"
+                  >
+                    <span className="sr-only">Open user menu</span>
+                    <img
+                      className="w-8 h-8 rounded-full"
+                      src="https://www.pngitem.com/pimgs/m/80-800194_transparent-users-icon-png-flat-user-icon-png.png"
+                      alt="user img"
+                    />
+                  </button>
+                  {/* <!-- Dropdown menu --> */}
+                  <div
+                    className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
+                    id="user-dropdown"
+                  >
+                    <div className="px-4 py-3">
+                      <span className="block text-sm text-gray-900 dark:text-white">
+                        Bonnie Green
+                      </span>
+                      <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">
+                        name@flowbite.com
+                      </span>
+                    </div>
+                    <ul
+                      className="py-2"
+                      aria-labelledby="user-menu-2"
+                      id="mobile-menu-2"
+                    >
+                      <li>
+                        <Link
+                          to="/dashboard"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        >
+                          Dashboard
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="/"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        >
+                          Settings
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="/"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        >
+                          Earnings
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="/"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        >
+                          Sign out
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
+                  <button
+                    data-collapse-toggle="mobile-menu-2"
+                    type="button"
+                    className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+                    aria-controls="mobile-menu-2"
+                    aria-expanded="false"
+                  >
+                    <span className="sr-only">Open main menu</span>
+                    <svg
+                      className="w-6 h-6"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </button>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="hidden lg:block">
+                  <Link to="/login">
+                    <button className="py-2.5 px-5 ml-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">
+                      Login
+                    </button>
+                  </Link>
+                </div>
+              </>
+            )}
+
             {/* login and profile */}
           </div>
           {/* search bar section ends here */}
@@ -267,97 +367,3 @@ const Header = () => {
 };
 
 export default Header;
-
-{
-  /* <!-- Profile menu --> */
-}
-// <div className="flex items-center md:order-2 ml-4">
-//   <button
-//     type="button"
-//     className="flex mr-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-//     id="user-menu-button"
-//     aria-expanded="false"
-//     data-dropdown-toggle="user-dropdown"
-//     data-dropdown-placement="bottom"
-//   >
-//     <span className="sr-only">Open user menu</span>
-//     <img
-//       className="w-8 h-8 rounded-full"
-//       src="/docs/images/people/profile-picture-3.jpg"
-//       alt="user img"
-//     />
-//   </button>
-//   <!-- Dropdown menu -->
-//   <div
-//     className="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-//     id="user-dropdown"
-//   >
-//     <div className="px-4 py-3">
-//       <span className="block text-sm text-gray-900 dark:text-white">
-//         Bonnie Green
-//       </span>
-//       <span className="block text-sm font-medium text-gray-500 truncate dark:text-gray-400">
-//         name@flowbite.com
-//       </span>
-//     </div>
-//     <ul className="py-2" aria-labelledby="user-menu-button">
-//       <li>
-//         <Link
-//           to="/dashboard"
-//           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-//         >
-//           Dashboard
-//         </Link>
-//       </li>
-//       <li>
-//         <Link
-//           to="/"
-//           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-//         >
-//           Settings
-//         </Link>
-//       </li>
-//       <li>
-//         <Link
-//           to="/"
-//           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-//         >
-//           Earnings
-//         </Link>
-//       </li>
-//       <li>
-//         <Link
-//           to="/"
-//           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-//         >
-//           Sign out
-//         </Link>
-//       </li>
-//     </ul>
-//   </div>
-//   <button
-//     data-collapse-toggle="mobile-menu-2"
-//     type="button"
-//     className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-//     aria-controls="mobile-menu-2"
-//     aria-expanded="false"
-//   >
-//     <span className="sr-only">Open main menu</span>
-//     <svg
-//       className="w-6 h-6"
-//       aria-hidden="true"
-//       fill="currentColor"
-//       viewBox="0 0 20 20"
-//       xmlns="http://www.w3.org/2000/svg"
-//     >
-//       <path
-//         fillRule="evenodd"
-//         d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-//         clipRule="evenodd"
-//       ></path>
-//     </svg>
-//   </button>
-// </div>;
-{
-  /* <!-- Profile menu --> */
-}
