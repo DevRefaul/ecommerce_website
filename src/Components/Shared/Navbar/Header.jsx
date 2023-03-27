a5e75e7e7244eed341d61d02c0ad80a218f764a0
DevRefaul <devrefaul@gmail.com>
2023-03-27 16:27:21 +0600

    removed unneccessary things from user menu

--- src/Components/Shared/Navbar/Header.jsx
+++ src/Components/Shared/Navbar/Header.jsx
@@ -136,24 +136,8 @@ const Header = () => {
                           Dashboard
                         </Link>
                       </li>
-                      <li>
-                        <Link
-                          to="/"
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                        >
-                          Settings
-                        </Link>
-                      </li>
-                      <li>
-                        <Link
-                          to="/"
-                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-                        >
-                          Earnings
-                        </Link>
-                      </li>
                       <li
-                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white cursor-pointer"
                         onClick={handleSignOut}
                       >
                         Sign out
