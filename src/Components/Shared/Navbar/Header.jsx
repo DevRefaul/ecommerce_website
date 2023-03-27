3f459a1ba9ff92e26ce080384426b9ebb7ee23b4
DevRefaul <devrefaul@gmail.com>
2023-03-27 16:07:07 +0600

    some changes made realted to login btn on navabr

--- src/Components/Shared/Navbar/Header.jsx
+++ src/Components/Shared/Navbar/Header.jsx
@@ -349,18 +349,20 @@ const Header = () => {
               {localStorage.getItem("UserLoggednIn") === "true" ? (
                 <></>
               ) : (
-                <li className="mt-4">
-                  <div className="lg:hidden">
-                    <Link to="/login">
-                      <button
-                        className="py-2.5 px-5 text-sm font-medium text-gray-900 focus:outline-none bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                        style={{ margin: "0px" }}
-                      >
-                        Login
-                      </button>
-                    </Link>
-                  </div>
-                </li>
+                <>
+                  <li className="mt-4">
+                    <div className="lg:hidden">
+                      <Link to="/login">
+                        <button
+                          className="py-2.5 px-5 rounded text-sm font-medium text-gray-900 focus:outline-none bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                          style={{ margin: "0px" }}
+                        >
+                          Login
+                        </button>
+                      </Link>
+                    </div>
+                  </li>
+                </>
               )}
             </ul>
           </div>
