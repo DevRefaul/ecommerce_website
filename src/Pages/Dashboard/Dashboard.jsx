107580b988256d1822bcad9b5ce2288b6b7ff513
DevRefaul <devrefaul@gmail.com>
2023-03-28 14:52:54 +0600

    some minor changes

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Sidebar from "./Sidebar/Sidebar";
 
 const Dashboard = () => {
+  // useEffect(() => {
+  //   window.location.reload();
+  // }, []);
+
   return (
     <>
       <Sidebar />
