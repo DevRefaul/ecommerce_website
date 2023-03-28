ab82a74e9a01000856c87a96b05bece3fa8a4e9e
DevRefaul <devrefaul@gmail.com>
2023-03-28 16:15:25 +0600

    added another link for orders in mobile nav

--- src/Components/Shared/Navbar/Header.jsx
+++ src/Components/Shared/Navbar/Header.jsx
@@ -338,14 +338,24 @@ const Header = () => {
               </li>
 
               {localStorage.getItem("UserLoggedIn") === "true" ? (
-                <li>
-                  <Link
-                    to="/profile"
-                    className="block md:hidden py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-                  >
-                    Profile
-                  </Link>
-                </li>
+                <>
+                  <li>
+                    <Link
+                      to="/profile"
+                      className="block md:hidden py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    >
+                      Profile
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      to="/orders"
+                      className="block md:hidden py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    >
+                      Orders
+                    </Link>
+                  </li>
+                </>
               ) : (
                 <li className="mt-4">
                   <div className="lg:hidden">
