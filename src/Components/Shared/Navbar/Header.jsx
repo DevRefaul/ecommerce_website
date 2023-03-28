8ed6d0037d300111e71d2b4cff2de84265fc3065
DevRefaul <devrefaul@gmail.com>
2023-03-28 16:21:08 +0600

    minor changes in navbar

--- src/Components/Shared/Navbar/Header.jsx
+++ src/Components/Shared/Navbar/Header.jsx
@@ -130,10 +130,18 @@ const Header = () => {
                     >
                       <li>
                         <Link
-                          to="/dashboard"
+                          to="/profile"
                           className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                         >
-                          Dashboard
+                          Profile
+                        </Link>
+                      </li>
+                      <li>
+                        <Link
+                          to="/orders"
+                          className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        >
+                          Orders
                         </Link>
                       </li>
                       <li
