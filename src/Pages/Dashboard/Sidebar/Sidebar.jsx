e20ca788017fab37ef4b70da4c802c76f6fbe343
DevRefaul <devrefaul@gmail.com>
2023-03-28 14:24:49 +0600

    minor change in siidebar signout btn

--- src/Pages/Dashboard/Sidebar/Sidebar.jsx
+++ src/Pages/Dashboard/Sidebar/Sidebar.jsx
@@ -138,7 +138,7 @@ const Sidebar = () => {
                 </span>
               </Link>
             </li>
-            <li>
+            <li className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
               <svg
                 aria-hidden="true"
                 className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
