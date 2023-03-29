fa88942e38aa1d53699ed76307c424df0f0085ed
DevRefaul <devrefaul@gmail.com>
2023-03-29 15:03:06 +0600

    added some texts for user information

--- src/Pages/Dashboard/Profile/Profile.jsx
+++ src/Pages/Dashboard/Profile/Profile.jsx
@@ -4,7 +4,14 @@ const Profile = () => {
   return (
     <section className="container mx-auto grid grid-cols-1 md:grid-cols-[1fr,2fr] xl:grid-cols-[1fr,3fr] gap-4">
       <div className="bg-green-400 h-28"></div>
-      <div className="bg-red-400 h-28"></div>
+      <div className="">
+        <h4 className="text-2xl font-semibold text-orange-500 mb-4">
+          Personal Information
+        </h4>
+
+        {/* user info */}
+        <div></div>
+      </div>
     </section>
   );
 };
