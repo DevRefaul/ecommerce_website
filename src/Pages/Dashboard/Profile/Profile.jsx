b5c8bad8445b75d5cc8ea79ed02acb2177cf2caf
DevRefaul <devrefaul@gmail.com>
2023-03-29 14:59:05 +0600

    updating profile page

--- src/Pages/Dashboard/Profile/Profile.jsx
+++ src/Pages/Dashboard/Profile/Profile.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Profile = () => {
-  return <div>THis is profile page</div>;
+  return (
+    <section className="container mx-auto grid grid-cols-1 md:grid-cols-[1fr,2fr] xl:grid-cols-[1fr,3fr] gap-4">
+      <div className="bg-green-400 h-28"></div>
+      <div className="bg-red-400 h-28"></div>
+    </section>
+  );
 };
 
 export default Profile;
