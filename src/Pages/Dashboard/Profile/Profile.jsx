8f7beb706038de7514c332052dadc72b1091e9ca
DevRefaul <devrefaul@gmail.com>
2023-03-29 16:10:42 +0600

    some changes are made in modal as need

--- src/Pages/Dashboard/Profile/Profile.jsx
+++ src/Pages/Dashboard/Profile/Profile.jsx
@@ -164,8 +164,9 @@ const Profile = () => {
       <button
         data-modal-target="staticModal"
         data-modal-toggle="staticModal"
-        class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        class="hidden text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
+        id="modalId"
       >
         Toggle modal
       </button>
@@ -184,7 +185,7 @@ const Profile = () => {
             {/* <!-- Modal header --> */}
             <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
               <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                Static modal
+                Enter Password Before Proceed
               </h3>
               <button
                 type="button"
@@ -207,18 +208,17 @@ const Profile = () => {
             </div>
             {/* <!-- Modal body --> */}
             <div class="p-6 space-y-6">
-              <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                With less than a month to go before the European Union enacts
-                new consumer privacy laws for its citizens, companies around the
-                world are updating their terms of service agreements to comply.
-              </p>
-              <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                The European Union’s General Data Protection Regulation
-                (G.D.P.R.) goes into effect on May 25 and is meant to ensure a
-                common set of data rights in the European Union. It requires
-                organizations to notify users as soon as possible of high-risk
-                data breaches that could personally affect them.
-              </p>
+              <div className="flex justify-between items-center">
+                <label htmlFor="password" className="inline-block w-[100px]">
+                  Password :
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="rounded w-full"
+                />
+              </div>
             </div>
             {/* <!-- Modal footer --> */}
             <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
@@ -227,14 +227,14 @@ const Profile = () => {
                 type="button"
                 class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                I accept
+                Proceed
               </button>
               <button
                 data-modal-hide="staticModal"
                 type="button"
                 class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
               >
-                Decline
+                Cancel
               </button>
             </div>
           </div>
