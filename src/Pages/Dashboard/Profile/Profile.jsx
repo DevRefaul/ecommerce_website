748b83c64bb5a8ece5da7ae68b4b9e7fb3e89051
DevRefaul <devrefaul@gmail.com>
2023-03-29 15:23:19 +0600

    added update info btn

--- src/Pages/Dashboard/Profile/Profile.jsx
+++ src/Pages/Dashboard/Profile/Profile.jsx
@@ -69,9 +69,11 @@ const Profile = () => {
                 className="rounded w-full"
               />
             </div>
-            <div className="flex justify-center my-4">
-              <button className="">Update Information</button>
-            </div>
+          </div>
+          <div className="flex justify-center my-4">
+            <button className="text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">
+              Update Information
+            </button>
           </div>
         </div>
       </div>
