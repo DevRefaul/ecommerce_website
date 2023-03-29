f1c5a78cc7ec6b5b6b0c1d6a2f3d0c485fb314d8
DevRefaul <devrefaul@gmail.com>
2023-03-29 15:22:03 +0600

    updating personal information section

--- src/Pages/Dashboard/Profile/Profile.jsx
+++ src/Pages/Dashboard/Profile/Profile.jsx
@@ -2,15 +2,78 @@ import React from "react";
 
 const Profile = () => {
   return (
-    <section className="container mx-auto grid grid-cols-1 md:grid-cols-[1fr,2fr] xl:grid-cols-[1fr,3fr] gap-4">
-      <div className="bg-green-400 h-28"></div>
-      <div className="">
-        <h4 className="text-2xl font-semibold text-orange-500 mb-4">
-          Personal Information
-        </h4>
+    <section className=" bg-orange-50">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-[1fr,2fr] xl:grid-cols-[1fr,3fr] gap-4">
+        <div className="bg-green-400 h-28"></div>
+        <div className="p-12">
+          <h4 className="text-2xl font-semibold text-orange-500 mb-4">
+            Personal Information
+          </h4>
+          <p className="my-6">
+            Manage your personal information icluding your phone number, address
+            and name . So that you can be contacted. <br />
+            !!! Please note that Email can not be changed
+          </p>
 
-        {/* user info */}
-        <div></div>
+          {/* user info */}
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6 bg-white px-6 py-12 border border-orange-500 rounded">
+            {/* user name */}
+            <div className="flex justify-between items-center">
+              <label htmlFor="name" className="inline-block w-[100px]">
+                Name :
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="rounded w-full"
+              />
+            </div>
+
+            {/* user email */}
+            <div className="flex justify-between items-center">
+              <label htmlFor="email" className="inline-block w-[100px]">
+                Email :
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                disabled
+                className="rounded w-full bg-orange-50"
+              />
+            </div>
+
+            {/* user phone */}
+            <div className="flex justify-between items-center">
+              <label htmlFor="phone" className="inline-block w-[100px]">
+                Phone :
+              </label>
+              <input
+                type="text"
+                name="phone"
+                id="phone"
+                className="rounded w-full"
+              />
+            </div>
+
+            {/* user address */}
+            <div className="flex justify-between items-center">
+              <label htmlFor="address" className="inline-block w-[100px]">
+                Address :
+              </label>
+              <input
+                type="text"
+                name="address"
+                id="address"
+                className="rounded w-full"
+              />
+            </div>
+            <div className="flex justify-center my-4">
+              <button className="">Update Information</button>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
