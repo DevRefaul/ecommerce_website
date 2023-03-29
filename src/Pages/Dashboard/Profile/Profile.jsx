fcd97246fa8a65bfd9f78103d46e76780d1e8957
DevRefaul <devrefaul@gmail.com>
2023-03-29 16:27:28 +0600

    added function when close btns are clicked

--- src/Pages/Dashboard/Profile/Profile.jsx
+++ src/Pages/Dashboard/Profile/Profile.jsx
@@ -191,6 +191,7 @@ const Profile = () => {
                 type="button"
                 className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
                 data-modal-hide="staticModal"
+                onClick={() => toast.error("Failed To Update Info")}
               >
                 <svg
                   className="w-5 h-5"
@@ -233,6 +234,7 @@ const Profile = () => {
                 data-modal-hide="staticModal"
                 type="button"
                 className="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600"
+                onClick={() => toast.error("Failed To Update Info")}
               >
                 Cancel
               </button>
