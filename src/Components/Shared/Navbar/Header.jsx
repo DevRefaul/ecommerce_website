95a2fcb2037cd37c03dc5e399cfa72e9b61b45ae
DevRefaul <devrefaul@gmail.com>
2023-04-01 14:48:34 +0600

    removed deleted imported css file for header

--- src/Components/Shared/Navbar/Header.jsx
+++ src/Components/Shared/Navbar/Header.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../../Assets/shopaholic.png";
-import "./Header.css";
 
 const Header = () => {
   const [dropdown, setDropdown] = useState(true);
