190be3e923e1e078c288c375088808e7d737e270
DevRefaul <devrefaul@gmail.com>
2023-04-02 15:55:20 +0600

    added table for cart items

--- src/Components/Shared/Navbar/Header.jsx
+++ src/Components/Shared/Navbar/Header.jsx
@@ -451,7 +451,52 @@ const Header = () => {
           <span className="sr-only">Close Cart</span>
         </button>
         <div className="py-4 overflow-y-auto">
-          {/* order will show here */}
+          {/* table starts here for showing cart items */}
+          <div class="relative overflow-x-auto">
+            <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+              <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                <tr>
+                  <th scope="col" class="px-6 py-3">
+                    Product name
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    Price
+                  </th>
+                  <th scope="col" class="px-6 py-3"></th>
+                  <th scope="col" class="px-6 py-3"></th>
+                </tr>
+              </thead>
+              <tbody>
+                {/* order will show here */}
+                {data?.cartItems?.map((item) => (
+                  <tr
+                    class="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+                    key={item._id}
+                  >
+                    <th
+                      scope="row"
+                      class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      {item.name}
+                    </th>
+                    <td class="px-6 py-4">{item.price}</td>
+                    <td class="px-6 py-4">
+                      <button className="p-2 rounded bg-orange-500 text-white font-semibold">
+                        Buy_Now
+                      </button>
+                    </td>
+                    <td class="px-6 py-4">
+                      <button className="p-2 rounded bg-red-500 text-white font-semibold">
+                        Remove
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          {/* table starts here for showing cart items */}
+
           {/* order will show here */}
         </div>
       </div>
