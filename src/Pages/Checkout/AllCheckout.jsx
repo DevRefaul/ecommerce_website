8b45e2a731c0fed3c68517e4088638b9131e7871
DevRefaul <devrefaul@gmail.com>
2023-04-03 03:50:53 +0600

    created useeffect for fetching cart items data

--- src/Pages/Checkout/AllCheckout.jsx
+++ src/Pages/Checkout/AllCheckout.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { Context } from "../../Utils/Contexts";
+import { api } from "../../Utils/Api";
 
 const AllCheckout = () => {
+  const { loadCartItems } = useContext(Context);
+
+  useEffect(() => {
+    fetch(`${api}/getcartitems`)
+      .then((res) => res.json())
+      .then((data) => data);
+  }, [loadCartItems]);
+
   return (
     <div>
-      <h2>This is all checkout pagegi</h2>
+      <h2>This is all checkout page</h2>
     </div>
   );
 };
