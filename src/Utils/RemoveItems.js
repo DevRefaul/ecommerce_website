50338666aac8d6f7b78979e58555e6829cd37f91
DevRefaul <devrefaul@gmail.com>
2023-04-02 19:41:32 +0600

    change on the delete item from cart function

--- src/Utils/RemoveItems.js
+++ src/Utils/RemoveItems.js
@@ -1,12 +1,21 @@
 import { api } from "./Api";
+import { toast } from "react-toastify";
 
-export const handleDeleteItemFromCart = (item) => {
 
+export const handleDeleteItemFromCart = (item, loadCartItems, setLoadCartItems) => {
 
     fetch(`${api}/removeitemfromcart`, {
         method: "DELETE",
         body: JSON.stringify(item)
     })
         .then(res => res.json())
-        .then(data => console.log(data))
+        .then(data => {
+            console.log(data);
+            if (data.deleteResponse.acknowledged) {
+                setLoadCartItems(!loadCartItems)
+                toast.success(data.message)
+            } else {
+                toast.error(data.message)
+            }
+        })
 };
