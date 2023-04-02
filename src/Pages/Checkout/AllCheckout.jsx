5dcfa4ee174a40847711ef5e02e14c94b7ead6d8
DevRefaul <devrefaul@gmail.com>
2023-04-03 04:27:03 +0600

    some changes in allchekout pag

--- src/Pages/Checkout/AllCheckout.jsx
+++ src/Pages/Checkout/AllCheckout.jsx
@@ -1,14 +1,15 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Context } from "../../Utils/Contexts";
 import { api } from "../../Utils/Api";
 
 const AllCheckout = () => {
   const { loadCartItems } = useContext(Context);
+  const [cartItems, setCartItems] = useState(null);
 
   useEffect(() => {
     fetch(`${api}/getcartitems`)
       .then((res) => res.json())
-      .then((data) => data);
+      .then((data) => setCartItems(data.cartItems));
   }, [loadCartItems]);
 
   return (
