4112f4be845ab893fd605cc398e678e161be1a61
DevRefaul <devrefaul@gmail.com>
2023-04-04 22:05:15 +0600

    updated order page

--- src/Pages/Dashboard/Orders/Orders.jsx
+++ src/Pages/Dashboard/Orders/Orders.jsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { Context } from "../../../Utils/Contexts";
 import { api } from "../../../Utils/Api";
 import {
