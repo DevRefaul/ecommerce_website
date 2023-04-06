f8b3806cf595f87a5d5e62c5c2754b54034eaa79
DevRefaul <devrefaul@gmail.com>
2023-04-06 21:31:07 +0600

    added sucess payment page

---
+++ src/Pages/Payment/SuccessPayment.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SuccessPayment = () => {
+  return (
+    <div>
+      <h2>Your Order Has Been Placed</h2>
+    </div>
+  );
+};
+
+export default SuccessPayment;
