28ee10a466882d635d6530832046bc949b05646f
DevRefaul <devrefaul@gmail.com>
2023-04-08 14:52:30 +0600

    checkout message page is completed

--- src/Pages/Payment/CheckoutMessage.jsx
+++ src/Pages/Payment/CheckoutMessage.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
+import Lottie from "lottie-react";
+
+import congratsAnim from "./checkoutAnim.json";
 
 const CheckoutMessage = () => {
   const location = useLocation();
@@ -7,14 +10,38 @@ const CheckoutMessage = () => {
   const { transactionId, totalPaid } = location.state;
 
   return (
-    <section className="container mx-auto py-10">
-      <h2 className="text-center text-2xl font-semibold">
-        Congratulation Your Payment Has Completed
-      </h2>
-      <p>
-        Your Transaction id id - {transactionId} and total bill is - {totalPaid}{" "}
-        $
-      </p>
+    <section className="container mx-auto py-10 flex items-center justify-center min-h-screen">
+      <div>
+        <div className="w-24 h-24 mx-auto">
+          <Lottie animationData={congratsAnim} />
+        </div>
+
+        <h2 className="text-center text-2xl font-semibold mt-10">
+          Congratulation Your Payment Has Completed
+        </h2>
+        <p className="text-center my-10">
+          Your Transaction id is -{" "}
+          <span className="text-lg font-semibold text-green-500">
+            {transactionId}
+          </span>{" "}
+          and total bill is -{" "}
+          <span className="text-lg font-semibold text-orange-500">
+            {totalPaid}
+          </span>{" "}
+          $
+        </p>
+        <p className="text-center text-2xl font-semibold my-10">
+          Save your transaction id for future
+        </p>
+
+        <div className="flex justify-center">
+          <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              Go To Homepage
+            </span>
+          </button>
+        </div>
+      </div>
     </section>
   );
 };
