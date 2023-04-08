7d1274c712d722b4c490ad453711af8e8135a0d0
DevRefaul <devrefaul@gmail.com>
2023-04-08 14:59:25 +0600

    added link for navigating to home page

--- src/Pages/Payment/CheckoutMessage.jsx
+++ src/Pages/Payment/CheckoutMessage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import Lottie from "lottie-react";
 
 import congratsAnim from "./checkoutAnim.json";
@@ -35,11 +35,13 @@ const CheckoutMessage = () => {
         </p>
 
         <div className="flex justify-center">
-          <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-            <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
-              Go To Homepage
-            </span>
-          </button>
+          <Link to="/">
+            <button class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+              <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Go To Homepage
+              </span>
+            </button>
+          </Link>
         </div>
       </div>
     </section>
