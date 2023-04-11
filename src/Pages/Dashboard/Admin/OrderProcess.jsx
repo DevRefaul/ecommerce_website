25106455415a11cd53ccaffb423254d323d56a99
DevRefaul <devrefaul@gmail.com>
2023-04-11 16:15:24 +0600

    created orders process page for admin

---
+++ src/Pages/Dashboard/Admin/OrderProcess.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OrderProcess = () => {
+  return (
+    <section className="py-8">
+      <h3>Orders</h3>
+    </section>
+  );
+};
+
+export default OrderProcess;
