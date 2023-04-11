f6dd6a7b75e9b2e78ac63af20a3226a2bad6e55c
DevRefaul <devrefaul@gmail.com>
2023-04-11 16:21:47 +0600

    updated the link route for orders in menu

--- src/Components/Shared/Navbar/Header.jsx
+++ src/Components/Shared/Navbar/Header.jsx
@@ -157,7 +157,7 @@ const Header = ({ user }) => {
                       {user?.role === "admin" && (
                         <li>
                           <Link
-                            to="/orders"
+                            to="/processorders"
                             className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
                           >
                             Orders
@@ -397,7 +397,7 @@ const Header = ({ user }) => {
                   {user?.role === "admin" && (
                     <li>
                       <Link
-                        to="/orders"
+                        to="/processorders"
                         className="block md:hidden py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                       >
                         Orders
