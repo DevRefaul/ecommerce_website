a8f83b17d92b3faef9615c235c83f82dbbb800ee
DevRefaul <devrefaul@gmail.com>
2023-04-11 16:19:20 +0600

    fetching all orders for process

--- src/Pages/Dashboard/Admin/OrderProcess.jsx
+++ src/Pages/Dashboard/Admin/OrderProcess.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { api } from "../../../Utils/Api";
 
 const OrderProcess = () => {
+  const [orders, setOrders] = useState();
+
+  useEffect(() => {
+    fetch(`${api}/orders`)
+      .then((res) => res.json())
+      .then((data) => setOrders(data.orders));
+  }, []);
+
+  console.log(orders);
+
   return (
     <section className="py-8">
-      <h3>Orders</h3>
+      <h3>Process Orders</h3>
     </section>
   );
 };
