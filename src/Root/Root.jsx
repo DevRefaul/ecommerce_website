08f98b08dd1dfe4566d27c25c8bb3e020a62449c
DevRefaul <devrefaul@gmail.com>
2023-04-11 15:35:59 +0600

    fetching user data in root

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,12 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import Footer from "../Components/Shared/Footer/Footer";
 import Header from "../Components/Shared/Navbar/Header";
+import { api } from "../Utils/Api";
+import TransParentLoadingScene from "../Components/LoadingScene/TransParentLoadingScene";
 
 const Root = () => {
+  const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState("");
+
+  useEffect(() => {
+    const userDetails = JSON.parse(localStorage.getItem("UserDetails"));
+    const userMail = userDetails?.email;
+
+    if (userMail) {
+      setLoading(true);
+      fetch(`${api}/getuserdata?email=${userMail}`)
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.status === 200 && data.userData._id) {
+            const user = data.userData;
+            setUser(user);
+            setLoading(false);
+          } else {
+            return;
+          }
+        });
+    }
+  }, []);
+
+  // loader scene when function wills work for api calls
+  if (loading) {
+    return <TransParentLoadingScene />;
+  }
+
   return (
     <>
-      <Header />
+      <Header user={user} />
       <Outlet />
       <Footer />
     </>
