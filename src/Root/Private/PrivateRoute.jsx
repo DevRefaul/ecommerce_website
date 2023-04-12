515df89f96a46552cf84ec63740d3dfe6b8d7645
DevRefaul <devrefaul@gmail.com>
2023-04-12 19:44:49 +0600

    created private route

---
+++ src/Root/Private/PrivateRoute.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+
+const PrivateRoute = ({ children }) => {
+  const navigate = useNavigate();
+
+  const user = JSON.parse(localStorage.getItem("UserDetails"));
+  const userLoggedIn = localStorage.getItem("UserLoggedIn");
+
+  if (!user.email && !user.name && userLoggedIn !== "true") {
+    toast.info("Please Login To Continue");
+    return navigate("/login");
+  }
+
+  return children;
+};
+
+export default PrivateRoute;
