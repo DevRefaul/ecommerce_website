24dd04ee211f152a9afae4c86ea6285f92a456c1
DevRefaul <devrefaul@gmail.com>
2023-04-20 23:24:59 +0600

    updated some styling change

--- src/Components/SingleCardComponents/SingleCardForAllProducts.jsx
+++ src/Components/SingleCardComponents/SingleCardForAllProducts.jsx
@@ -12,50 +12,52 @@ const SingleCardForAllProducts = ({ product }) => {
 
   return (
     <>
-      <div className="max-w-sm h-[530px] lg:h-[500px] 2xl:h-[450px] singleCard">
+      <div className="max-w-sm h-full singleCard">
         <ToastContainer />
         <Card
           imgSrc={image && image}
           imgAlt={name ? `${name + "'s Image"}` : "Product Image"}
-          className="h-[530px] lg:h-[500px] 2xl:h-[450px] "
+          className=" "
         >
-          <div>
-            <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white flex justify-between">
-              {name?.length > 15 ? name.slice(0, 15) + "..." : name}
-              {brand && (
-                <Badge color="success" size="sm">
-                  {brand}
-                </Badge>
-              )}
-            </h5>
+          <div className="flex flex-col justify-between">
+            <div>
+              <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white flex justify-between">
+                {name?.length > 15 ? name.slice(0, 15) + ".." : name}
+                {brand && (
+                  <Badge color="success" size="sm">
+                    {brand}
+                  </Badge>
+                )}
+              </h5>
 
-            <p className="font-normal text-start text-gray-700 dark:text-gray-400">
-              {description.slice(0, 50) + "..."}
-            </p>
+              <p className="font-normal text-start text-gray-700 dark:text-gray-400">
+                {description.slice(0, 50) + "..."}
+              </p>
 
-            <h4 className="text-xl font-bold text-start mt-4">
-              Price: {price}$
-            </h4>
-          </div>
+              <h4 className="text-xl font-bold text-start mt-4">
+                Price: {price}$
+              </h4>
+            </div>
 
-          {/* section for price */}
-          <div id="bottomBtns" className="grid lg:grid-cols-2 gap-2">
-            <Link
-              to={`/products/${category}/${name}`}
-              state={{ productName: name, productId: _id }}
-            >
-              <button className="seeMore text-sm py-2.5 text-center mr-2 mb-2 lg:mt-6 w-full">
-                See More
+            {/* section for btns */}
+            <div id="bottomBtns" className="grid xl:grid-cols-2 gap-2">
+              <Link
+                to={`/products/${category}/${name}`}
+                state={{ productName: name, productId: _id }}
+              >
+                <button className="seeMore text-sm py-2.5 text-center mr-2 mb-2 mt-6 xl:mt-4 w-full">
+                  See More
+                </button>
+              </Link>
+              <button
+                className="addToCart text-sm p-0 text-center ml-2 mb-2 xl:mt-4 w-full"
+                onClick={() =>
+                  addOrderToDB(product, loadCartItems, setLoadCartItems)
+                }
+              >
+                Add_To_Cart
               </button>
-            </Link>
-            <button
-              className="addToCart text-sm py-2.5 text-center mr-2 mb-2 lg:mt-6 w-full"
-              onClick={() =>
-                addOrderToDB(product, loadCartItems, setLoadCartItems)
-              }
-            >
-              Add To Cart
-            </button>
+            </div>
           </div>
         </Card>
       </div>
