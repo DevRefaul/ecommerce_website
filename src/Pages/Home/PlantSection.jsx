8b37b2598b9fc47bf24bbb24cb21898d98de1388
DevRefaul <devrefaul@gmail.com>
2023-04-20 16:01:43 +0600

    updated buttons style of categorys sections

--- src/Pages/Home/PlantSection.jsx
+++ src/Pages/Home/PlantSection.jsx
@@ -36,7 +36,7 @@ const PlantSection = () => {
       </div>
 
       <Link to="/products/plant">
-        <button className="text-white bg-gradient-to-r from-green-300 to-blue-400 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-6">
+        <button className="seeMore text-sm px-5 py-2.5 text-center mr-2 mb-2 mt-6">
           See More Products
         </button>
       </Link>
