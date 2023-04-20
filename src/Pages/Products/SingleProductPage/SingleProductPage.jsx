24015f61a5399121b49924769e7baecbbde03518
DevRefaul <devrefaul@gmail.com>
2023-04-20 16:09:35 +0600

    added class for cart btn

--- src/Pages/Products/SingleProductPage/SingleProductPage.jsx
+++ src/Pages/Products/SingleProductPage/SingleProductPage.jsx
@@ -108,7 +108,7 @@ const SingleProductPage = () => {
           </h2>
 
           <button
-            className="text-white bg-gradient-to-br from-green-400 to-blue-600 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-8 py-2.5 text-center mr-2 mb-2"
+            className="seeMore text-sm px-8 py-2.5 text-center mr-2 mb-2"
             onClick={() =>
               addOrderToDB(product, loadCartItems, setLoadCartItems)
             }
