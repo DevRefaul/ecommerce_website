3adebb306d169d1f4d9f3930b2a4728892a8c3f1
DevRefaul <devrefaul@gmail.com>
2023-04-20 16:13:05 +0600

    writted style for buy now button

--- src/Pages/Products/SingleProductPage/SingleProductPage.jsx
+++ src/Pages/Products/SingleProductPage/SingleProductPage.jsx
@@ -116,7 +116,7 @@ const SingleProductPage = () => {
             Add To Cart
           </button>
           <Link to={`/order/${productId}`} state={{ product }}>
-            <button className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-8 py-2.5 text-center mr-2 mb-2">
+            <button className="buyNow text-sm px-8 py-2.5 text-center mr-2 mb-2">
               Buy Now
             </button>
           </Link>
