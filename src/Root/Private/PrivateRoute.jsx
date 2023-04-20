3c692e06a0ec9b6e4c73fc30208ee555b4455e46
DevRefaul <devrefaul@gmail.com>
2023-04-20 23:06:01 +0600

    private route is working fine now

--- src/Root/Private/PrivateRoute.jsx
+++ src/Root/Private/PrivateRoute.jsx
@@ -7,7 +7,6 @@ const PrivateRoute = ({ children }) => {
   const user = JSON.parse(localStorage.getItem("UserDetails"));
   const userLoggedIn = localStorage.getItem("UserLoggedIn");
 
-  console.log("hitted here");
 
   if (!user || !user.email || !user.name || userLoggedIn !== "true") {
     toast.info("Please Login To Continue");
