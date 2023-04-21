199e6dc0b801c7df90ea201a5db0457a4cbd23e0
DevRefaul <devrefaul@gmail.com>
2023-04-21 14:54:56 +0600

    updated nav positions for dropdown

--- src/Components/Shared/Navbar/Header.jsx
+++ src/Components/Shared/Navbar/Header.jsx
@@ -379,7 +379,7 @@ const Header = ({ user }) => {
                 {/* <!-- Dropdown menu --> */}
                 <div
                   id="dropdownNavbar"
-                  className="z-50 absolute top-14 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 md:left-20"
+                  className="z-50 absolute left-[7rem] top-[160px] lg:top-[2.5rem] hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600 lg:left-[18rem] xl:left-[28rem] 2xl:left-[35rem]"
                 >
                   <ul
                     className="py-2 text-sm text-gray-700 dark:text-gray-400"
@@ -468,8 +468,8 @@ const Header = ({ user }) => {
                   )}
                   <li className="lg:hidden">
                     <button
-                      className="py-2.5 px-5 rounded text-sm font-medium text-gray-900 focus:outline-none bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                      style={{ margin: "0px" }}
+                      className="py-2 px-2 rounded text-sm font-medium text-gray-900 focus:outline-none bg-white border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                      style={{ margin: "8px 0px 0px" }}
                     >
                       Sign Out
                     </button>
