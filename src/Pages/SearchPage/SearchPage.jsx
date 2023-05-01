9ddb4c841ab8524c93c928da036a95382fdc5d36
DevRefaul <devrefaul@gmail.com>
2023-05-01 12:29:04 +0600

    some changes are done on search page and header

--- src/Pages/SearchPage/SearchPage.jsx
+++ src/Pages/SearchPage/SearchPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SearchPage = () => {
-  return <div></div>;
+    return (
+      <div>
+        <h2>This is search result page</h2>
+      </div>
+    );
 };
 
 export default SearchPage;
