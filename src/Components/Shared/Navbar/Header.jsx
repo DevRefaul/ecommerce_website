b42aa9e547bdd136756e380af054398b95f732d9
DevRefaul <devrefaul@gmail.com>
2023-05-01 12:15:29 +0600

    updated search btn for mobile

--- src/Components/Shared/Navbar/Header.jsx
+++ src/Components/Shared/Navbar/Header.jsx
@@ -322,7 +322,7 @@ const Header = ({ user }) => {
             id="navbar-search"
           >
             <div className="relative mt-3 lg:hidden">
-              <div className="absolute inset-y-0 right-0 flex items-center pl-3 pointer-events-none">
+              <div className="absolute top-0 bottom-0 right-0 flex items-center px-3 cursor-pointer rounded-r-lg bg-sky-200">
                 <svg
                   className="w-5 h-5 text-gray-500"
                   aria-hidden="true"
@@ -340,7 +340,7 @@ const Header = ({ user }) => {
               <input
                 type="text"
                 id="search-navbar"
-                className="block w-full p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                className="block w-full p-2 pr-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Search..."
               />
             </div>
